386f1a9a7ebfd7b250607af2ac46c0de7ddc132a
N1115697 <giovanni.wells2022@my.ntu.ac.uk>
2025-05-21 16:23:24 +0100

    Added stub definitions of the main files i beleve ill need in this project

---
+++ Pong/Ball.cpp
@@ -0,0 +1,19 @@
+#include "Ball.h"
+#include <iostream>
+using namespace std;
+
+class Ball {
+private:
+    double Width;
+    double Height;
+    double Postion;
+public:
+
+    Ball() {
+        Width = 0.2;
+        Height = 0.2;
+    }
+
+
+};
+
---
+++ Pong/Paddle.cpp
@@ -0,0 +1,19 @@
+#include "Paddle.h"
+#include <iostream>
+using namespace std;
+
+class Paddle {
+private:
+    double Width;
+    double Height;
+    double Postion;
+public:
+
+    Paddle() {
+        Width = 0.2;
+        Height = 1;
+    }
+
+
+};
+
